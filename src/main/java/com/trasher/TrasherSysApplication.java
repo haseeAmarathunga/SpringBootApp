31d0485656fc41de6fb5db47f20c25967516d319
haseeAmarathunga <haseeamarathunga@gmail.com>
2019-02-21 20:09:34 +0530

    Initial App

---
+++ src/main/java/com/trasher/TrasherSysApplication.java
@@ -0,0 +1,13 @@
+package com.trasher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrasherSysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrasherSysApplication.class, args);
+	}
+
+}
